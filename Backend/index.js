2d8471820a9b99e5120ba40e1999d560c308c586
Sourav <souravsahu70047@gmail.com>
2023-05-12 21:11:12 +0530

    Added Starter for server in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,19 @@
-console.log("Welcome")
+const express=require("express");
+
+const app=express();
+
+const cors=require("cors");
+
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to App");
+})
+
+app.listen(process.env.port,async()=>{
+    try{
+        console.log("Connected to DB");
+    }catch(err){
+        console.log("Something went wrong");
+    }
+})
