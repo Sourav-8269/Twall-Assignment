aa53ea8535105de7895e9c232cf9926c45efa94e
Sourav <souravsahu70047@gmail.com>
2023-05-13 21:28:35 +0530

    Added Tasks Page

--- Frontend/twall/src/App.js
+++ Frontend/twall/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Tasks from './Components/Tasks';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Tasks/>
+      <Navbar/>
+      {/* <Tasks/> */}
     </div>
   );
 }
--- Frontend/twall/src/Components/Tasks.jsx
+++ Frontend/twall/src/Components/Tasks.jsx
@@ -15,62 +15,7 @@ const Tasks = () => {
 
   return (
     <Box mt={["20%", "15%", "8%"]}>
-      <Box w="80%" m="auto">
-        <Heading size="lg" as="h1" fontWeight="" textAlign={"left"} mb="3%">
-          Sprint 28 March - 5 April{" "}
-        </Heading>
-      </Box>
-      <table>
-        <thead>
-          <tr>
-            <th>Task</th>
-            <th>Description</th>
-            <th>Status</th>
-            <th>Edit Task</th>
-            <th>Delete Task</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data &&
-            data.length > 0 &&
-            data.map((el) => (
-              <tr key={el._id}>
-                <td>{el.title}</td>
-                <td>{el.description}</td>
-                <td
-                  style={{
-                    backgroundColor: `${el.status ? "green" : "#ECC94B"}`,
-                    color: "white",
-                  }}
-                >
-                  {el.status ? "Done" : "Pending"}
-                </td>
-                <td
-                  style={{
-                    backgroundColor: "#1da1f2",
-                    color: "white",
-                    fontWeight: "bold",
-                    cursor: "pointer",
-                  }}
-                //   onClick={() => navigate(`/edit/${el._id}`)}
-                >
-                  Edit
-                </td>
-                <td
-                  style={{
-                    backgroundColor: "red",
-                    color: "white",
-                    fontWeight: "bold",
-                    cursor: "pointer",
-                  }}
-                //   onClick={() => handleDelete(el._id)}
-                >
-                  Delete
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      
     </Box>
   );
 };
