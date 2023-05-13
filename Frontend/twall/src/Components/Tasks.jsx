6206bf0f10a9d4e5900b9c142c549849ff833c9d
Sourav <souravsahu70047@gmail.com>
2023-05-13 22:10:59 +0530

    Added Files

--- Frontend/twall/src/Components/Tasks.jsx
+++ Frontend/twall/src/Components/Tasks.jsx
@@ -1,21 +1,41 @@
 import React, { useEffect } from "react";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Container, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 import { useState } from "react";
-import axios from "axios"
-import "../Styles/Tasks.css"
+import axios from "axios";
+import "../Styles/Tasks.css";
 
 const Tasks = () => {
-  const [data,setData]=useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/task")
-    .then((res)=>setData(res.data))
-    .catch((err)=>console.log(err))
+    axios
+      .get("http://localhost:8080/task")
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return (
     <Box mt={["20%", "15%", "8%"]}>
-      
+      {/* Previous width={["60%","80%"]} for below Box */}
+      <Box width="80%" m="auto" border="0px solid red" >
+        <SimpleGrid
+          minChildWidth="250px"
+          spacing="40px"
+          marginTop="30px"
+          textAlign="center"
+        >
+          {data.length > 0 &&
+            data.map((el) => (
+              <Box boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px" borderRadius="25px" padding="10%" textAlign="left">
+                <Heading>{el.title}</Heading>
+                <Text>{el.description}</Text>
+              </Box>
+            ))}
+        </SimpleGrid>
+      </Box>
     </Box>
   );
 };
