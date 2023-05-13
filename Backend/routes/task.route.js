98c4d86a917897597f90c547da31a9301827fe7e
Sourav <souravsahu70047@gmail.com>
2023-05-13 15:57:59 +0530

    Added All Requests in Backend

--- Backend/routes/task.route.js
+++ Backend/routes/task.route.js
@@ -15,9 +15,6 @@ TaskRouter.get("/",async(req,res)=>{
 
 TaskRouter.post("/add",async(req,res)=>{
     let payload=req.body;
-    if(!payload){
-        return res.send("Please send all details");
-    }
     try{
         const task=new TaskModel(payload);
         await task.save();
@@ -31,24 +28,18 @@ TaskRouter.post("/add",async(req,res)=>{
 TaskRouter.patch("/edit/:id",async(req,res)=>{
     const id=req.params.id;
     const payload=req.body;
-    if(!payload||!id){
-        return res.send("Please send all details");
-    }
     try{
-        await TaskModel.findByIdAndUpdate({_id:id},payload);
+        await TaskModel.findByIdAndUpdate({_id:id},payload,{new:true});
         res.send(`Updated Task with id ${id}`);
     }catch{
-
+        res.send("Something Went Wrong");
+        console.log(err);
     }
 })
 
 TaskRouter.put("/replace/:id",async (req,res)=>{
     const id=req.params.id
     const payload=req.body;
-    // console.log(id)
-    if(!payload||!id){
-        return res.send("Please send all details");
-    }
     try{
         await TaskModel.findOneAndReplace({_id:id},payload);
         res.send(`Replaced Todo with id ${id}`);
@@ -60,10 +51,6 @@ TaskRouter.put("/replace/:id",async (req,res)=>{
 
 TaskRouter.delete("/delete/:id",async (req,res)=>{
     const id=req.params.id
-    // console.log(id)
-    if(!id){
-        return res.send("Please send all details");
-    }
     try{
         await TaskModel.findByIdAndDelete({_id:id});
         res.send(`Deleted Todo with id ${id}`);
