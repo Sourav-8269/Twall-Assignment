b7ebf4300e22c692f7b3fff6ed327475dbe40016
Sourav <souravsahu70047@gmail.com>
2023-05-14 13:17:14 +0530

    Added UI in Tasks Page

--- Frontend/twall/src/Components/Tasks.jsx
+++ Frontend/twall/src/Components/Tasks.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
-import { Box, Container, Heading, SimpleGrid, Text, useColorMode } from "@chakra-ui/react";
+import { Box, Container, Heading,HStack, SimpleGrid, Text, Tooltip, useColorMode } from "@chakra-ui/react";
 import { useState } from "react";
 import axios from "axios";
 import "../Styles/Tasks.css";
+import {CheckIcon, CloseIcon, DeleteIcon, EditIcon} from "@chakra-ui/icons";
 
 const Tasks = () => {
   const [data, setData] = useState([]);
@@ -37,7 +38,7 @@ const Tasks = () => {
                 borderRadius="25px"
                 padding="10%"
                 textAlign="left"
-                bg={el._status?"#68D391":"#4299E1"}
+                bg={el.status?"#68D391":"#4299E1"}
               >
                 {/* <Heading>{el.title}</Heading> */}
                 <Text
@@ -49,9 +50,23 @@ const Tasks = () => {
                 <Text fontFamily="sans-serif" fontSize={"20px"}>
                   - {el.description}
                 </Text>
-                <Text fontFamily="sans-serif" fontSize={"20px"} color={el.status?"#48BB78":"#F56565"} >
+                <Text fontFamily="sans-serif" fontSize={"20px"} color={el.status?"":"#F56565"} >
                   Status : {el.status?"Completed":"Not Completed"}
                 </Text>
+                <HStack mt="2%" p="3%" justifyContent="space-between"  >
+                  <Tooltip label="Edit Task" hasArrow placement='bottom-start'>
+                    <EditIcon cursor="pointer" boxSize={6}/>
+                  </Tooltip>
+                  <Tooltip label="Delete Task" hasArrow placement='bottom-start'>
+                    <DeleteIcon cursor="pointer" boxSize={6}/>
+                  </Tooltip>
+                  {!el.status&&<Tooltip label="Completed" hasArrow placement='bottom-start'>
+                    <CheckIcon cursor="pointer" boxSize={6}/>
+                  </Tooltip>}
+                  {el.status&&<Tooltip label="Not Completed" hasArrow placement='bottom-start'>
+                    <CloseIcon cursor="pointer" boxSize={6}/>
+                  </Tooltip>}
+                </HStack>
               </Box>
             ))}
         </SimpleGrid>
